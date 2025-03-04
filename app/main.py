cc0151b18af44b4cb1f3b49e35bd75bd019b980f
DamikaAlwis <damikaa.20@cse.mrt.ac.lk>
2025-03-04 19:38:27 +0530

    remove context manageer from main.py

--- app/main.py
+++ app/main.py
@@ -1,21 +1,12 @@
 from fastapi import FastAPI
-from .routers import ads
+from app.routers import ads as ads_router
 import logging
 from app.logging_config import setup_logging
+
 
 setup_logging()
 logger = logging.getLogger(__name__)
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    # Start up tasks
-    print("Application startup...")
-    
-    yield
-    
-    # Clean up tasks
-    print("Application shutdown...")
-
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 
 app.include_router(ads_router, prefix="/ads", tags=["ads"])
