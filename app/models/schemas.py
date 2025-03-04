cde48ff1e024fc0bad7f62bc09d83f6be7ba2e63
DamikaAlwis <damikaa.20@cse.mrt.ac.lk>
2025-03-04 10:30:04 +0530

    changed Ad shema

--- app/models/schemas.py
+++ app/models/schemas.py
@@ -1,12 +1,53 @@
-from typing import List, Dict, Any, Optional
+from typing import List, Dict, Any
 from pydantic import BaseModel
+from enum import Enum
 
 
+class MainCategory(str, Enum):
+    ELECTRONICS = 'electronics'
+    PROPERTY = 'property'
+    VEHICLE = 'vehicle'
+
+
+class SubCategory(str, Enum):
+    VAN = 'van'
+    THREE_WHEELER = 'three-wheeler'
+    ROOM_ANNEX = 'room & annex'
+    LORRY_TRUCK = 'lorry truck'
+    LAND = 'land'
+    HOUSE = 'house'
+    COMMERCIAL_PROPERTY = 'commercial property'
+    CAR = 'car'
+    BIKE = 'bike'
+    BICYCLE = 'bicycle'
+    APARTMENT = 'apartment'
+    TVS = 'tvs'
+    MOBILE_PHONES_TABLETS = 'mobile phones & tablets'
+    MOBILE_PHONE_ACCESSORIES = 'mobile phone accessories'
+    ELECTRONIC_HOME_APPLIANCES = 'electronic home appliances'
+    COMPUTERS = 'computers'
+    COMPUTER_ACCESSORIES = 'computer accessories'
+    CAMERAS_CAMCORDERS = 'cameras & camcorders'
+    AUDIO_MP3 = 'audio & mp3'
+    AIR_CONDITIONS_ELECTRICAL_FITTINGS = 'air conditions & electrical fittings'
+
+
+class TransactionType(str, Enum):
+    SALE = "sale"
+    RENT = "rent"
+
+
+class WantedOffering(str, Enum):
+    OFFERING = "offering"
+    WANTED = "wanted"
 class Ad(BaseModel):
     """Schema for an ad"""
-    # title: str
-    description: str
-    # metadata: Optional[Dict[str, Any]] = None
+    text: str
+    main_category : MainCategory
+    sub_category : SubCategory
+    transaction_type : TransactionType = TransactionType.SALE
+    wanted_offering : WantedOffering = WantedOffering.OFFERING
+
 
 
 class AdsRequest(BaseModel):
