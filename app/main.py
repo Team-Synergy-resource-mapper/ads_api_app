941db5c82d8f56d3a5d2b02457698693801b00fc
DamikaAlwis <damikaa.20@cse.mrt.ac.lk>
2025-03-04 10:36:05 +0530

    add logger to main file

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from .routers import ads
+import logging
+from app.logging_config import setup_logging
 
+setup_logging()
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
