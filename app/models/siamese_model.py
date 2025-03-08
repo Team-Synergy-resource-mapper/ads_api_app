36e2982d5e3ed68cb3f06981a9ebc4bf3fe9a9bc
DamikaAlwis <damikaa.20@cse.mrt.ac.lk>
2025-03-08 08:18:09 +0530

    added siamese model with categories

--- app/models/siamese_model.py
+++ app/models/siamese_model.py
@@ -1,10 +1,11 @@
 import tensorflow as tf
 from tensorflow import keras
 
+
+# L2 normalization function
 def l2_normalize_fn(x):
     """L2 normalization for unit-length embeddings"""
     return tf.math.l2_normalize(x, axis=1)
-
 
 @tf.keras.utils.register_keras_serializable()
 class SimpleAttentionFusion(tf.keras.layers.Layer):
@@ -14,13 +15,10 @@ class SimpleAttentionFusion(tf.keras.layers.Layer):
         super(SimpleAttentionFusion, self).__init__(**kwargs)
         self.units = units
         self.dropout_rate = dropout_rate
-        self.attention = None
-        self.dense = None
-        self.dropout = None
 
     def build(self, input_shape):
         self.attention = tf.keras.layers.Dense(
-            1, activation="gelu")  # Compute attention scores
+            input_shape[-1], activation="gelu")  # Feature-wise attention scores
         self.dense = tf.keras.layers.Dense(
             self.units, kernel_regularizer=tf.keras.regularizers.l2(1e-4))
         self.dropout = tf.keras.layers.Dropout(
@@ -28,16 +26,13 @@ class SimpleAttentionFusion(tf.keras.layers.Layer):
         super().build(input_shape)
 
     def call(self, inputs, training=None):
-        expanded_inputs = tf.expand_dims(
-            inputs, axis=1)  # (batch, 1, features)
-        scores = self.attention(expanded_inputs)  # (batch, 1, 1)
-        # Optional: Apply dropout to attention scores
-        scores = self.dropout(scores, training=training)
-        attention_weights = tf.nn.softmax(scores, axis=1)  # Normalize
-        context_vector = attention_weights * expanded_inputs  # Weighted features
-        output = self.dense(tf.squeeze(context_vector, axis=1))  # Project back
-        # Apply dropout to the output of the dense layer
-        output = self.dropout(output, training=training)
+        scores = self.attention(inputs)  # (batch, features)
+        scores = self.dropout(scores, training=training)  # Apply dropout
+        attention_weights = tf.nn.softmax(
+            scores, axis=-1)  # Normalize across features
+        context_vector = attention_weights * inputs  # Weighted feature representation
+        output = self.dense(context_vector)  # Project back
+        output = self.dropout(output, training=training)  # Apply dropout
         return output
 
     def get_config(self):
@@ -48,6 +43,55 @@ class SimpleAttentionFusion(tf.keras.layers.Layer):
     @classmethod
     def from_config(cls, config):
         return cls(**config)
+
+
+# def l2_normalize_fn(x):
+#     """L2 normalization for unit-length embeddings"""
+#     return tf.math.l2_normalize(x, axis=1)
+
+
+# @tf.keras.utils.register_keras_serializable()
+# class SimpleAttentionFusion(tf.keras.layers.Layer):
+#     """Simple attention mechanism to fuse important features"""
+
+#     def __init__(self, units=256, dropout_rate=0.1, **kwargs):
+#         super(SimpleAttentionFusion, self).__init__(**kwargs)
+#         self.units = units
+#         self.dropout_rate = dropout_rate
+#         self.attention = None
+#         self.dense = None
+#         self.dropout = None
+
+#     def build(self, input_shape):
+#         self.attention = tf.keras.layers.Dense(
+#             1, activation="gelu")  # Compute attention scores
+#         self.dense = tf.keras.layers.Dense(
+#             self.units, kernel_regularizer=tf.keras.regularizers.l2(1e-4))
+#         self.dropout = tf.keras.layers.Dropout(
+#             self.dropout_rate)  # Dropout layer
+#         super().build(input_shape)
+
+#     def call(self, inputs, training=None):
+#         expanded_inputs = tf.expand_dims(
+#             inputs, axis=1)  # (batch, 1, features)
+#         scores = self.attention(expanded_inputs)  # (batch, 1, 1)
+#         # Optional: Apply dropout to attention scores
+#         scores = self.dropout(scores, training=training)
+#         attention_weights = tf.nn.softmax(scores, axis=1)  # Normalize
+#         context_vector = attention_weights * expanded_inputs  # Weighted features
+#         output = self.dense(tf.squeeze(context_vector, axis=1))  # Project back
+#         # Apply dropout to the output of the dense layer
+#         output = self.dropout(output, training=training)
+#         return output
+
+#     def get_config(self):
+#         config = super(SimpleAttentionFusion, self).get_config()
+#         config.update({"units": self.units, "dropout_rate": self.dropout_rate})
+#         return config
+
+#     @classmethod
+#     def from_config(cls, config):
+#         return cls(**config)
 
 
 def load_siamese_branch(model_path):
